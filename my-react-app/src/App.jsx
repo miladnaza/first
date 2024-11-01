afa21572c31a49f3947d8b8bdeabe2a0f48abee1
miladnaza <milad.habibzada7@gmail.com>
2024-11-01 17:26:46 -0400

    i  completed my part

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -6,8 +6,10 @@ function App() {
       <h1>Team Member Profiles</h1>
       <>
       <h2>Milad Nazari</h2>
-      <p>Date Of Birth: 1995-01-01</p>
-      
+      <p>Date Of Birth: 2000-10-30</p>
+      <p>Favorite food:pizza</p>
+      <p>Favorite Emoji:🦁</p>
+      <>Birthplace:Kabul</>
       </>
     </div>
   );
