91bfa4bf42e614a927ac96eecab423feccce1063
miladnaza <milad.habibzada7@gmail.com>
2024-11-01 18:14:58 -0400

    complete

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,26 +2,22 @@ import React from 'react';
 
 function App() {
   return (
-    <div >
+    <div class="milad">
       <h1>Team Member Profiles</h1>
-      <>
+      <div class="box">
       <h2>Milad Nazari</h2>
       <p><strong>Date Of Birth:</strong> 2000-10-30</p>
       <p><strong>Favorite food:</strong>pizza</p>
       <p><strong>Favorite Emoji:</strong>🦁</p>
-      <><strong>Birthplace:</strong>Kabul</>
-      
-      
-      </>
-      <>
+      <p><strong>Birthplace:</strong>Kabul</p>
+      </div>
+      <div class="box">
         <h2>Michael Valde</h2>
         <p><strong>Date Of Birth:</strong>1994-04-19</p>
         <p><strong>Favorite Food:</strong>Cake</p>
         <p><strong>Favorite Emoji:</strong>🍔</p>
         <p><strong>Birthplace:</strong>Earth</p>
-        
-        
-      </>
+      </div>
     </div>
   )
 }
