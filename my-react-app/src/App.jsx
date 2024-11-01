b715961fdc7a7e547eaf60d7baac01f187e962d8
miladnaza <milad.habibzada7@gmail.com>
2024-11-01 17:14:56 -0400

    Update App.jsx

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -3,7 +3,11 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
+<<<<<<< Updated upstream
       <h1>Hello, MiKE king  /h1>
+=======
+      <h1>Hello, Milad Nazari king </h1>
+>>>>>>> Stashed changes
     </div>
   );
 }
